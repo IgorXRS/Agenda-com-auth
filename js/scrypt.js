63aef0fd2fbde5b4439617454d9eaa51ddc87fa6
Igor Xavier <igorxrds@gmail.com>
2023-12-20 07:55:08 -0300

    Update

---
+++ js/scrypt.js
@@ -0,0 +1,145 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBK0svcRvD6tDvCl2HAwpTyTAV9mLjnOBM",
+    authDomain: "cotacao-for.firebaseapp.com",
+    projectId: "cotacao-for",
+    storageBucket: "cotacao-for.appspot.com",
+    messagingSenderId: "81640818285",
+    appId: "1:81640818285:web:b7ef058d2dc2ffdca52164",
+    measurementId: "G-HLB64D6QGK"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Começamos aqui
+
+var usuario = null;
+
+var formLogin = document.querySelector('form.login-form');
+var btnLogout = document.querySelector('.logout');
+    
+formLogin.addEventListener('submit',(e)=>{
+e.preventDefault();
+    let email = document.querySelector('[name=email]').value;
+    let password = document.querySelector('[name=password]').value;
+    //alert(email);
+    //alert(password);
+        
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in
+        usuario = userCredential.user;
+            
+        //alert('Logado com sucesso! '+usuario.email);
+        document.querySelector('.login, .background-login').style.display = "none";
+        document.querySelector('.container-login').style.display = "block";
+        document.querySelector('.background-logado').style.display = "flex";
+
+        formLogin.reset();
+        
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+    });
+    });
+
+const db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged((val)=>{
+
+if(val){
+    usuario=val;
+    //alert('Bem-vindo de volta '+ usuario.email);
+
+    document.querySelector('.login, .background-login').style.display = "none";
+    document.querySelector('.container-login').style.display = "block";
+    document.querySelector('.background-logado').style.display = "flex";
+
+    //Ouvir por mudanças no banco de dados.
+
+    db.collection('tarefas').where("horario","!=",null).onSnapshot((data)=>{
+        let list = document.querySelector('#tarefas');
+        list.innerHTML = "";
+        let tarefas = data.docs;
+        tarefas = tarefas.sort(function(a,b){
+            if(a.data().horario < b.data().horario)
+                return -1;
+            else
+                return +1;
+            })
+        tarefas.map((val)=>{
+            list.innerHTML+=`<li>${val.data().tarefa} <a tarefa-id="${val.id}" class="excluir-btn" href="javascript:void(0)"><i class="bi bi-trash"></i></a></li>`
+        })
+
+        var excluirTarefas = document.querySelectorAll('.excluir-btn');
+
+        excluirTarefas.forEach(element => {
+            element.addEventListener('click',function(e){
+                e.preventDefault();
+                let docId = element.getAttribute('tarefa-id');
+
+                db.collection('tarefas').doc(docId).delete();
+            })
+        });
+    })
+}
+})
+
+
+
+
+btnLogout.addEventListener('click',(e)=>{
+e.preventDefault();
+
+    firebase.auth().signOut().then(() => {
+        usuario = null;
+        document.querySelector('.login').style.display = "block";
+        document.querySelector('.background-login').style.display = "Flex";
+        document.querySelector('.container-login, .background-logado').style.display = "none";
+            
+       //alert('Deslogado');
+    }).catch((error) => {
+        // An error happened.
+    });
+})
+
+
+
+var formCadastro = document.querySelector('.form-cadastro-tarefa');
+
+
+
+formCadastro.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   
+   let tarefa = document.querySelector('.form-cadastro-tarefa [name=tarefa]').value;
+   let dateTime = document.querySelector('.form-cadastro-tarefa [name=datetime]').value;
+
+   //Inserir e criar coleção caso não existe.
+
+   let dataAtual = new Date().getTime();
+   if(dataAtual > new Date(dateTime).getTime()){
+    alert('Você informou uma data no passado...')
+   }else if(tarefa == "" || dateTime == ""){
+        alert('Você não pode deixar os campos vázios...')
+   }else{
+        db.collection('tarefas').add({
+            tarefa: tarefa,
+            horario: new Date(dateTime).getTime(),
+            userId: usuario.uid
+        })
+        formCadastro.reset();
+        alert('Sua tarefa foi cadastrada com sucesso!')
+   }
+})
+
+  
